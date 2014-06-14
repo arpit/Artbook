dfa315cdbca1d2389446c9587276b3696457d886
Arpit Mathur <mathur.arpit@gmail.com>
2014-06-13 22:24:36 -0400

    first commit, changed namespace from Comcast

---
+++ src/com/arpitonline/freeflow/artbook/ArtbookActivity.java
@@ -0,0 +1,144 @@
+package com.arpitonline.freeflow.artbook;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.arpitonline.freeflow.artbook.data.DribbbleDataAdapter;
+import com.arpitonline.freeflow.artbook.layouts.ArtbookLayout;
+import com.arpitonline.freeflow.artbook.models.DribbbleFeed;
+import com.arpitonline.freeflow.artbook.models.DribbbleFetch;
+import com.comcast.freeflow.core.AbsLayoutContainer;
+import com.comcast.freeflow.core.AbsLayoutContainer.OnItemClickListener;
+import com.comcast.freeflow.core.FreeFlowContainer;
+import com.comcast.freeflow.core.FreeFlowContainer.OnScrollListener;
+import com.comcast.freeflow.core.FreeFlowItem;
+import com.comcast.freeflow.layouts.FreeFlowLayout;
+import com.comcast.freeflow.layouts.HLayout;
+import com.comcast.freeflow.layouts.VGridLayout;
+import com.comcast.freeflow.layouts.VLayout;
+
+public class ArtbookActivity extends Activity implements OnClickListener{
+
+	public static final String TAG = "ArtbookActivity";
+
+	private FreeFlowContainer container;
+	private VGridLayout grid;
+	private ArtbookLayout custom;
+
+	private DribbbleFetch fetch;
+	private int itemsPerPage = 25;
+	private int pageIndex = 1;
+	
+	DribbbleDataAdapter adapter;
+	
+	FreeFlowLayout[] layouts;
+	int currLayoutIndex = 0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_artbook);
+		
+
+		container = (FreeFlowContainer) findViewById(R.id.container);
+
+		Display display = getWindowManager().getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		
+		findViewById(R.id.load_more).setOnClickListener(this);
+		//Our new layout
+		custom = new ArtbookLayout();
+		
+		//Grid Layout
+		grid = new VGridLayout();
+		VGridLayout.LayoutParams params = new VGridLayout.LayoutParams(size.x/2, size.x/2);
+		grid.setLayoutParams(params);
+		
+		//Vertical Layout
+		VLayout vlayout = new VLayout();
+		VLayout.LayoutParams params2 = new VLayout.LayoutParams(size.x);
+		vlayout.setLayoutParams(params2);
+		
+		//HLayout
+		HLayout hlayout = new HLayout();
+		hlayout.setLayoutParams(new HLayout.LayoutParams(size.x));
+		
+		
+		layouts = new FreeFlowLayout[]{custom, grid, vlayout, hlayout};
+		
+		adapter = new DribbbleDataAdapter(this);
+		
+		
+		container.setLayout(layouts[currLayoutIndex]);
+		container.setAdapter(adapter);
+		
+		
+		fetch = new DribbbleFetch();
+		
+		fetch.load(this,itemsPerPage , pageIndex);
+
+	}
+
+	public void onDataLoaded(DribbbleFeed feed) {
+		Log.d(TAG, "photo: " + feed.getShots().get(0).getImage_teaser_url());
+		adapter.update(feed);
+		container.dataInvalidated();
+		container.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AbsLayoutContainer parent, FreeFlowItem proxy) {
+				
+			}
+		});
+		
+		container.addScrollListener( new OnScrollListener() {
+			 
+			@Override
+			public void onScroll(FreeFlowContainer container) {
+				Log.d(TAG, "scroll percent "+ container.getScrollPercentY() );
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.artbook, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		switch(item.getItemId()){
+		case (R.id.action_change_layout):
+			currLayoutIndex++;
+			if(currLayoutIndex == layouts.length){
+				currLayoutIndex =  0;
+			}
+			container.setLayout(layouts[currLayoutIndex]);
+			
+			break;
+		case (R.id.action_about): Intent about = new Intent(this, AboutActivity.class);
+			startActivity(about);
+			break;
+		}
+		
+		return true;
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.d(TAG, "Loading data");
+		pageIndex++;
+		fetch.load(this, itemsPerPage, pageIndex);
+	}
+}
---
+++ src/com/arpitonline/freeflow/artbook/models/DribbbleFetch.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright 2013 Comcast Cable Communications Management, LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.arpitonline.freeflow.artbook.models;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import com.arpitonline.freeflow.artbook.ArtbookActivity;
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class DribbbleFetch {
+	
+	public static final String TAG = "DribbbleFetch";
+
+	
+	public void load(final ArtbookActivity caller, int itemsPerPage, int page) {
+		
+		new AsyncTask<String, Void, String>() {
+
+			OkHttpClient client = new OkHttpClient();
+
+			private Exception exception;
+
+			protected String doInBackground(String... urls) {
+				try {
+					return get(new URL(urls[0]));
+
+				} catch (Exception e) {
+					this.exception = e;
+					Log.e(TAG, "Exception: " + e);
+					return null;
+				}
+			}
+
+			protected void onPostExecute(String data) {
+				DribbbleFeed feed  = new Gson().fromJson(data, DribbbleFeed.class);
+				caller.onDataLoaded(feed);
+			}
+
+			String get(URL url) throws IOException {
+				HttpURLConnection connection = client.open(url);
+				InputStream in = null;
+				try {
+					in = connection.getInputStream();
+					byte[] response = readFully(in);
+					return new String(response, "UTF-8");
+				} finally {
+					if (in != null)
+						in.close();
+				}
+			}
+
+			byte[] readFully(InputStream in) throws IOException {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				byte[] buffer = new byte[1024];
+				for (int count; (count = in.read(buffer)) != -1;) {
+					out.write(buffer, 0, count);
+				}
+				return out.toByteArray();
+			}
+
+		}.execute("http://api.dribbble.com/shots/popular?per_page="+itemsPerPage+"&page="+page);
+	}
+
+}
