82647fb7631ebedc57a670cd37ebbdda6a318c1e
Arpit Mathur <mathur.arpit@gmail.com>
2014-09-02 19:43:44 -0400

    Share working in details view

--- src/com/arpitonline/freeflow/artbook/DetailsActivity.java
+++ src/com/arpitonline/freeflow/artbook/DetailsActivity.java
@@ -1,23 +1,39 @@
 package com.arpitonline.freeflow.artbook;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.arpitonline.freeflow.artbook.models.Shot;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends DetailsCapableActivity {
 
 	private ViewPager pager;
-	private PagerAdapter adapter;
+	private PhotosDetailsListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +54,78 @@ public class DetailsActivity extends DetailsCapableActivity {
 	}
 	
 	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.details, menu);
+		return true;
+	}
+	
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		final  Shot s = adapter.getItem(pager.getCurrentItem());
 	    switch (item.getItemId()) {
 	    case android.R.id.home:
 	        NavUtils.navigateUpFromSameTask(this);
 	        return true;
+	    case R.id.open_in_browser:
+	    	String url = s.getUrl();
+	    	Intent i = new Intent(Intent.ACTION_VIEW);
+	    	i.setData(Uri.parse(url));
+	    	startActivity(i);
+	    	return true;
+	    	
+	    case R.id.share:
+	    	new AsyncTask<Void, Integer, Void>() {
+	    		
+				@Override
+				protected Void doInBackground(Void... params) {
+					Bitmap b = null;
+					try {
+						b = Picasso.with(DetailsActivity.this).load(s.getImage_url()).get();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+						return null;
+					}
+					String url = s.getImage_url();
+					String exten = url.substring(url.lastIndexOf(".")+1);
+					
+					Log.d("share", "extension: "+exten);
+					
+					Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+			    	sharingIntent.setType("image/"+exten);
+			    	sharingIntent.putExtra(Intent.EXTRA_TITLE, s.getTitle());
+			    	sharingIntent.putExtra(Intent.EXTRA_SUBJECT, s.getTitle());
+			    	String desc = Html.fromHtml(s.getDescription()).toString();
+			    	
+			    	sharingIntent.putExtra(Intent.EXTRA_TEXT, desc);
+			    	String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Artbook";
+			    	File dir = new File(file_path);
+			    	if(!dir.exists())
+			    		dir.mkdirs();
+			    	
+			    	
+			    	
+			    	
+			    	
+			    	File file = new File(dir, "image."+exten);
+			    	FileOutputStream fOut;
+			    	try {
+			    		fOut = new FileOutputStream(file);
+			    		b.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+			    		fOut.flush();
+			    		fOut.close();
+			    		
+			    		Log.d("share", "share file: "+file.getAbsolutePath());
+			    		
+			    		sharingIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+				    	startActivity(sharingIntent);
+			    		
+			    	} catch (Exception e) {
+			    		e.printStackTrace();
+			    	}
+					return null;
+				}
+	    		
+			}.execute();
 	    }
 	    return super.onOptionsItemSelected(item);
 	}
@@ -54,6 +137,10 @@ public class DetailsActivity extends DetailsCapableActivity {
 
 		public PhotosDetailsListAdapter(ArrayList<Shot> shots) {
 			this.shots = shots;
+		}
+		
+		public Shot getItem(int index){
+			return shots.get(index);
 		}
 
 		@Override
